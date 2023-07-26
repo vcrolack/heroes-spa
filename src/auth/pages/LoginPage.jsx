ba53100bb7ec069ba9a31196587680f2d3c4701a
Victor Rolack <victorolack@gmail.com>
2023-07-25 23:13:59 -0400

    This is the last commit

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,11 +1,21 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom"
+
+import { AuthContext } from "../context";
 
 
 export const LoginPage = () => {
   const navigate = useNavigate();
 
+  const { login } = useContext(AuthContext);
+
   const onLogin = () => {
-    navigate('/marvel', {
+
+    const lastPath = localStorage.getItem('lastPath') || '/';
+
+    login('Victor Rolack');
+
+    navigate(lastPath, {
       replace:true
     })
   }
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,13 +2,25 @@ import { Route, Routes } from "react-router-dom"
 
 import { LoginPage } from "../auth/pages"
 import { HeroesRoutes } from "../heroes/routes/HeroesRoutes"
+import { PrivateRouter } from "./PrivateRouter"
+import { PublicRouter } from "./PublicRouter"
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="login" element={ <LoginPage />} />
-        <Route path="/*" element={ <HeroesRoutes />} />
+        <Route path="login" element={
+          <PublicRouter>
+            <LoginPage/>
+          </PublicRouter>
+        } />
+
+
+        <Route path="/*" element={
+          <PrivateRouter>
+            <HeroesRoutes />
+          </PrivateRouter>
+        } />
       </Routes>
     </>
   )
